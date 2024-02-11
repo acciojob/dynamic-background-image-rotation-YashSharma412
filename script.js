5c9abf59a573d80402f61eda0a5ea32a2bd7b728
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2024-02-11 11:43:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const LandscapeBg = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg"
+const PortraitBg = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg"
+const body = document.querrySelector('body');
+
+function dynamicBackgroundImg(){
+	if (window.innerWidth > window.innerHeight) {
+		// Landscape Mode
+		body.style.backgroundImage = `url("${LandscapeBg}")`
+	} else {
+		
+		body.style.backgroundImage = `url("${PortraitBg}")`
+		
+	}
+}
